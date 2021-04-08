2e926e5a62191be4c3c3a031e7652db49dcae0c8
hang yin <hy2568@columbia.edu>
2021-04-07 22:02:04 -0500

    Added numberFormat.js to covert number to USD format.

--- src/Util/numberFormat.js
+++ src/Util/numberFormat.js
@@ -1,3 +1,6 @@
 const options = {style: 'currency', currency: 'USD'};
 export const numberFormat = new Intl.NumberFormat('en-US', options);
 
+export let formatValueToUSD = (value) => {
+    return numberFormat.format(value);
+}
