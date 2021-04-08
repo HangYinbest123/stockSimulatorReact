e6b0c6eccb6fe71e919c560e7ae87d35b56c331d
hang yin <hy2568@columbia.edu>
2021-04-07 22:01:36 -0500

    Added login.js to handle user login action

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import GoogleLogin from 'react-google-login';
-
+import './css/Login.css';
 
 export default class Login extends React.Component {
 
     successResponseGoogle = (googleUser) => {
         console.log(googleUser.getBasicProfile());
         console.log(googleUser.getAuthResponse());
-        let id_token = googleUser.getAuthResponse().id_token;
-        let userId = googleUser.getBasicProfile().getId();
-        this.props.onLoginSuccessfullyChange(userId);
+
+        this.props.onLoginSuccessfullyChange(googleUser);
     }
     failureResponseGoogle = (googleUser) => {
         console.log(googleUser);
