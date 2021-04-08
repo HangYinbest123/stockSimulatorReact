e0ed3bdda063aa02229b0f17555dd064e79eede2
Hang Yin <43092535+HangYinbest123@users.noreply.github.com>
2021-04-07 22:35:31 -0500

    Update YahooFinancial.js

--- src/api/YahooFinancial.js
+++ src/api/YahooFinancial.js
@@ -7,7 +7,7 @@ const GET_QUOTE_URL = "/market/v2/get-quotes";
 export const YahooFinancial = axios.create({
     baseURL: BASE_URL_YAHOO_FINANCE,
     headers: {
-        "x-rapidapi-key": "f38da746f4msh7f2e240c3bf8e90p1faf74jsnd6dcfe95eb41",
+        "x-rapidapi-key": "****************",
         "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
         "useQueryString": true
     },
