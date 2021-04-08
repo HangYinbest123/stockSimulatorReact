7fcf4aa97d0c062f0c15dc415acdfbe1dec2b3fd
hang yin <hy2568@columbia.edu>
2021-04-07 22:05:27 -0500

    Added feature to display user's stocks.

--- src/Component/StockCard.js
+++ src/Component/StockCard.js
@@ -1,12 +1,9 @@
 import React from "react";
 import AnimatedNumber from "animated-number-react";
-import {numberFormat} from '../Util/numberFormat';
-import './StockCard.css';
+import {formatValueToUSD} from '../Util/numberFormat';
+import './css/StockCard.css';
 
 export default class StockCard extends React.Component {
-    formatValue = (value) => {
-        return numberFormat.format(value, 0);
-    }
 
     render() {
         console.log("stocks props in StockCard");
@@ -14,16 +11,31 @@ export default class StockCard extends React.Component {
         return (
             <div className="stock-card">
                 <div className="ui segment">
-                    <div className="stock-card-name">{this.props.stock.longName}</div>
-                    <div className="stock-card-quote">
+                    <div className="stock-card-name">{this.props.stock.symbol}
+                        <span className="stock-card-quote" style={{float: "right"}}>
                         <AnimatedNumber
-                        value={this.props.stock.quote}
-                        formatValue={this.formatValue}
-                        duration={600}
+                            value={this.props.stock.quote}
+                            formatValue={formatValueToUSD}
+                            duration={600}
                         />
+                    </span>
                     </div>
-                    <div>Quantity</div>
-                    <div>{this.props.stock.quantity}</div>
+
+                    <div>
+                        Quantity
+                        <span style={{float: "right"}}>{this.props.stock.quantity}</span>
+                    </div>
+
+                    <div>
+                        Unit Cost
+                        <span style={{float: "right"}}>
+                            <AnimatedNumber
+                                value={this.props.stock.unitCost}
+                                formatValue={formatValueToUSD}
+                                duration={600}
+                            /></span>
+                    </div>
+
                 </div>
             </div>
         );
--- src/Component/StockCardList.js
+++ src/Component/StockCardList.js
@@ -1,19 +1,20 @@
 import React from "react";
 
 import StockCard from "./StockCard";
-import './StockCardList.css';
+import './css/StockCardList.css';
 
 export default class StockCardList extends React.Component {
     render() {
         console.log("stocks props in StockCardList");
         console.log(JSON.stringify(this.props.stocks));
         const stockCards = this.props.stocks.map(stock => {
-           return <StockCard key={stock.id} stock={stock}></StockCard>
+            if(stock.quantity != 0){
+                return <StockCard key={stock["recordId"]} stock={stock}></StockCard>
+            }
         });
         return (
             <div className="stock-card-list ui segments">
-                <h2>Your Stocks</h2>
-                <div>(Using fake data, API in development...)</div>
+                <h2>Your Stock List</h2>
                 {stockCards}
             </div>
         );
